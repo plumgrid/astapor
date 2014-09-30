3e5b025d3dca3fb8604febb94b4d27c2ebffc193
salmank <salmank@plumgrid.com>
2014-09-30 19:48:17 +0500

    Add iptables PG rules.
    Signed-off-by: salmank <salmank@plumgrid.com>

--- puppet/modules/quickstack/manifests/neutron/plugins/plumgrid.pp
+++ puppet/modules/quickstack/manifests/neutron/plugins/plumgrid.pp
@@ -2,17 +2,25 @@
 # === Parameters
 #
 class quickstack::neutron::plugins::plumgrid (
-  $package_ensure          = 'installed',
-  $pg_connection           = $quickstack::params::pg_connection,
-  $pg_director_server      = $quickstack::params::pg_director_server,
-  $pg_director_server_port = $quickstack::params::pg_director_server_port,
-  $pg_username             = $quickstack::params::pg_username,
-  $pg_password             = $quickstack::params::pg_password,
-  $pg_servertimeout        = $quickstack::params::pg_servertimeout,
+  $package_ensure           = 'installed',
+  $pg_connection            = $quickstack::params::pg_connection,
+  $pg_director_server       = $quickstack::params::pg_director_server,
+  $pg_director_server_port  = $quickstack::params::pg_director_server_port,
+  $pg_username              = $quickstack::params::pg_username,
+  $pg_password              = $quickstack::params::pg_password,
+  $pg_servertimeout         = $quickstack::params::pg_servertimeout,
   $pg_enable_metadata_agent = $quickstack::params::pg_enable_metadata_agent,
-
+  $pg_fw_src                = $quickstack::params::pg_fw_src,
+  $pg_fw_dest               = $quickstack::params::pg_fw_dest,
 ) inherits quickstack::params {
 
+  firewall { '001 plumgrid rpc':
+    proto       => 'tcp',
+    action      => 'accept',
+    state       => ['NEW'],
+    destination => $pg_fw_dest,
+    source      => $pg_fw_src,
+  }->
   class { '::neutron::plugins::plumgrid':
    package_ensure          => $package_ensure,
    connection              => $pg_connection,
